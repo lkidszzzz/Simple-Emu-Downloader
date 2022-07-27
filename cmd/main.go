5fd88782d661c1daac1c00b71ce4aa4d01fa4e45
lkidszzzz <yanjiang.cst@outlook.com>
2022-07-27 17:34:49 +0800

    main

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-var emu, yuzu string
+var emu, input string
 var breaker int
 
 func main() {
@@ -33,21 +33,24 @@ func main() {
 				fmt.Println("输入 3 选择Github公共CDN下载通道2（使用人数较多时可能速度较慢，请避开高峰期使用）。")
 				fmt.Println("输入 4 返回。")
 				fmt.Println("请输入对应的数字以选择下载通道：")
-				_, err := fmt.Scan(&yuzu)
+				_, err := fmt.Scan(&input)
 				if err != nil {
 					fmt.Println("Unknown error.")
 					return
 				}
-				e, err := strconv.Atoi(yuzu)
+				e, err := strconv.Atoi(input)
 				switch {
 				case e == 1:
-					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", get.YuzuGetter())
+					link := get.YuzuGetter()
+					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
 					breaker = 1
 				case e == 2:
-					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", get.YuzuCDN1())
+					link := get.YuzuCDN1()
+					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
 					breaker = 1
 				case e == 3:
-					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", get.YuzuCDN2())
+					link := get.YuzuCDN2()
+					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
 					breaker = 1
 				case e == 4:
 					breaker = 1
@@ -67,23 +70,126 @@ func main() {
 				fmt.Println("输入 3 选择Github公共CDN下载通道2（使用人数较多时可能速度较慢，请避开高峰期使用）。")
 				fmt.Println("输入 4 返回。")
 				fmt.Println("请输入对应的数字以选择下载通道：")
-				_, err := fmt.Scan(&yuzu)
+				_, err := fmt.Scan(&input)
 				if err != nil {
 					fmt.Println("Unknown error.")
 					return
 				}
-				e, err := strconv.Atoi(yuzu)
+				e, err := strconv.Atoi(input)
 				switch {
 				case e == 1:
-					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", get.RyuGetter())
+					link := get.RyuGetter()
+					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
 					breaker = 1
 				case e == 2:
-					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", get.RyuCDN1())
+					link := get.RyuCDN1()
+					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
 					breaker = 1
 				case e == 3:
-					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", get.RyuCDN2())
+					link := get.RyuCDN2()
+					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
 					breaker = 1
 				case e == 4:
+					breaker = 1
+				case err != nil || e < 1 || e > 4:
+					fmt.Println("Warn:请按要求正确输入数字。")
+				}
+				if breaker == 1 {
+					breaker = 0
+					break
+				}
+			}
+		case e == 3:
+			for {
+				fmt.Println("Citra3DS-下载：")
+				fmt.Println("输入 1 选择Citra-Nightly（更新较慢，相当于稳定版）；")
+				fmt.Println("输入 2 选择Citra-Canary（每日更新，相当于测试版）。")
+				fmt.Println("输入 3 返回。")
+				fmt.Println("请输入对应的数字以选择您想要下载的版本：")
+				_, err := fmt.Scan(&input)
+				if err != nil {
+					fmt.Println("Unknown error.")
+					return
+				}
+				e, err := strconv.Atoi(input)
+				switch {
+				case e == 1:
+					for {
+						fmt.Println("Citra3DS-Nightly-下载：")
+						fmt.Println("输入 1 选择Github普通下载通道（国内部分地区可能体验不佳甚至无法访问,若自己有代理&加速服务则优先选择）；")
+						fmt.Println("输入 2 选择Github公共CDN下载通道1（推荐）（使用人数较多时可能速度较慢，请避开高峰期使用）。")
+						fmt.Println("输入 3 选择Github公共CDN下载通道2（使用人数较多时可能速度较慢，请避开高峰期使用）。")
+						fmt.Println("输入 4 返回。")
+						fmt.Println("请输入对应的数字以选择下载通道：")
+						_, err := fmt.Scan(&input)
+						if err != nil {
+							fmt.Println("Unknown error.")
+							return
+						}
+						e, err := strconv.Atoi(input)
+						switch {
+						case e == 1:
+							link := get.CitraNightlyGetter(get.CitraNightlyVerGetter())
+							fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
+							breaker = 1
+						case e == 2:
+							link := get.CitraNightlyCDN1()
+							fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
+							breaker = 1
+						case e == 3:
+							link := get.CitraNightlyCDN2()
+							fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
+							breaker = 1
+						case e == 4:
+							breaker = 1
+						case err != nil || e < 1 || e > 4:
+							fmt.Println("Warn:请按要求正确输入数字。")
+						}
+						if breaker == 1 {
+							breaker = 0
+							break
+						}
+						breaker = 1
+					}
+				case e == 2:
+					for {
+						fmt.Println("Citra3DS-Canary-下载：")
+						fmt.Println("输入 1 选择Github普通下载通道（国内部分地区可能体验不佳甚至无法访问,若自己有代理&加速服务则优先选择）；")
+						fmt.Println("输入 2 选择Github公共CDN下载通道1（推荐）（使用人数较多时可能速度较慢，请避开高峰期使用）。")
+						fmt.Println("输入 3 选择Github公共CDN下载通道2（使用人数较多时可能速度较慢，请避开高峰期使用）。")
+						fmt.Println("输入 4 返回。")
+						fmt.Println("请输入对应的数字以选择下载通道：")
+						_, err := fmt.Scan(&input)
+						if err != nil {
+							fmt.Println("Unknown error.")
+							return
+						}
+						e, err := strconv.Atoi(input)
+						switch {
+						case e == 1:
+							link := get.CitraCanaryGetter(get.CitraCanaryVerGetter())
+							fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
+							breaker = 1
+						case e == 2:
+							link := get.CitraCanaryCDN1()
+							fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
+							breaker = 1
+						case e == 3:
+							link := get.CitraCanaryCDN2()
+							fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
+							breaker = 1
+						case e == 4:
+							breaker = 1
+						case err != nil || e < 1 || e > 4:
+							fmt.Println("Warn:请按要求正确输入数字。")
+						}
+						if breaker == 1 {
+							breaker = 0
+							break
+						}
+						breaker = 1
+					}
+				case e == 3:
 					breaker = 1
 				case err != nil || e < 1 || e > 3:
 					fmt.Println("Warn:请按要求正确输入数字。")
@@ -93,12 +199,36 @@ func main() {
 					break
 				}
 			}
-		case e == 3:
-
 		case e == 4:
+			for {
+				fmt.Println("PPSSPP-下载：")
+				fmt.Println("输入 1 下载；")
+				fmt.Println("输入 2 返回。")
+				fmt.Println("请输入对应的数字以选择下载或返回：")
+				_, err := fmt.Scan(&input)
+				if err != nil {
+					fmt.Println("Unknown error.")
+					return
+				}
+				e, err := strconv.Atoi(input)
+				switch {
+				case e == 1:
+					link := get.PpssppGetter()
+					fmt.Println("下载链接（您也可以复制到您喜欢的下载器去进行下载）：", link)
+					breaker = 1
+				case e == 2:
+					breaker = 1
+				case err != nil || e < 1 || e > 2:
+					fmt.Println("Warn:请按要求正确输入数字。")
+				}
+				if breaker == 1 {
+					breaker = 0
+					break
+				}
+			}
 		case e == 5:
-			breaker = 1
 			fmt.Println("over!!!")
+			breaker = 1
 		case err != nil || e < 1 || e > 5:
 			fmt.Println("Warn:请按要求正确输入数字。")
 		}

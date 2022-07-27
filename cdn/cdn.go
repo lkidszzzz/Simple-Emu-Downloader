4d7965a4cd2a3575eeafa5ffb48207803cadcf9f
lkidszzzz <yanjiang.cst@outlook.com>
2022-07-27 16:37:36 +0800

    CDN下载加速

---
+++ cdn/cdn.go
@@ -0,0 +1,9 @@
+package cdn
+
+func GithubCdn1(link string) string {
+	return "https://ghproxy.com/" + link
+}
+
+func GithubCdn2(link string) string {
+	return "https://gh.api.99988866.xyz/" + link
+}

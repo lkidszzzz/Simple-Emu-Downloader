b2baa6a6f9ea337e2bf2809696420f7ba83862a8
lkidszzzz <yanjiang.cst@outlook.com>
2022-07-27 19:34:54 +0800

    Github公共CDN加速

--- cdn/cdn.go
+++
@@ -1,9 +0,0 @@
-package cdn
-
-func GithubCdn1(link string) string {
-	return "https://ghproxy.com/" + link
-}
-
-func GithubCdn2(link string) string {
-	return "https://gh.api.99988866.xyz/" + link
-}
